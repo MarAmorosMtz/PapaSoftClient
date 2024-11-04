2ecf30d508d37486a3dbe28f51414311b73642c8
kevin-luna <L21020397@veracruz.tecnm.mx>
2024-11-03 22:20:03 -0600

    Jackson mapper was configured to parse LocalDate datatype.

--- src/main/java/com/example/papasoftclient/utils/JsonMapper.java
+++ src/main/java/com/example/papasoftclient/utils/JsonMapper.java
@@ -1,12 +1,21 @@
 package com.example.papasoftclient.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.text.SimpleDateFormat;
 
 public class JsonMapper {
     private static ObjectMapper mapper;
 
     public static ObjectMapper getMapper(){
-        if(mapper == null){mapper = new ObjectMapper();}
+        if(mapper == null){
+            mapper = new ObjectMapper();
+            mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+            mapper.registerModule(new JavaTimeModule());
+            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        }
         return mapper;
     }
 }
