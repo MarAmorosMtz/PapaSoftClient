92b02e42457761af48d0a849d8d1c670403da203
kevin-luna <L21020397@veracruz.tecnm.mx>
2024-11-05 19:38:09 -0600

    Endpoint URLs added.

--- src/main/java/com/example/papasoftclient/repositories/RestAPI.java
+++ src/main/java/com/example/papasoftclient/repositories/RestAPI.java
@@ -1,10 +1,15 @@
 package com.example.papasoftclient.repositories;
 
 public class RestAPI {
-    public static final String HOST = "http://localhost:8000";
+    public static final String API_VERSION = "v0.5.0";
+    public static final String HOST = "http://localhost:8000/api/"+API_VERSION+"/";
     public static final String CARRERAS_ENDPOINT = HOST+"/carreras/";
     public static final String MATERIAS_ENDPOINT = HOST+"/materias/";
     public static final String MAESTROS_ENDPOINT = HOST+"/maestros/";
     public static final String PERIODOS_ENDPOINT = HOST+"/periodos/";
     public static final String SALONES_ENDPOINT = HOST+"/salones/";
+    public static final String ASESORADOS_ENDPOINT = HOST+"/asesorados/";
+    public static final String ASESORIAS_ENDPOINT = HOST+"/asesorias/";
+    public static final String HORARIOS_ASESORES_ENDPOINT = HOST+"/horarios/asesores";
+    public static final String HORARIOS_SALONES_ENDPOINT = HOST+"/horarios/salones";
 }
