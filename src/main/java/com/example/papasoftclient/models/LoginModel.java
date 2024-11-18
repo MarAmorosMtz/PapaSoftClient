72913a162c180b4b43b86c3fb57beacb2b5a9afb
Kevin Luna <L21020397@veracruz.tecnm.mx>
2024-11-18 12:05:17 -0600

    Some login logic was implemented.

---
+++ src/main/java/com/example/papasoftclient/models/LoginModel.java
@@ -0,0 +1,39 @@
+package com.example.papasoftclient.models;
+
+import com.example.papasoftclient.controllers.main.LoginController;
+
+public class LoginModel {
+    private String usuario;
+    private String clave;
+    private int tipo;
+
+    public LoginModel() {}
+
+    public LoginModel(String usuario, String clave, int tipo) {
+
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+}
